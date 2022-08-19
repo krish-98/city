3f5c267b1b56b139fce3a23eb7f235cdcc7810bc
krish-98 <muralikrishnanm98@gmail.com>
2022-08-19 19:22:13 +0530

    added some animation on the navbar using framer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,30 @@ import React from "react"
 import Logo from "../assets/logo.png"
 import { MdShoppingBasket } from "react-icons/md"
 import Avatar from "../assets/avatar2.png"
+import { motion } from "framer-motion"
 
 const Header = () => {
   return (
     <>
       {/* Mobile Navbar */}
-      <nav className="md:hidden flex items-center justify-between py-3 px-5 bg-primary">
-        <div className="relative cursor-pointer">
+      <nav className="md:hidden flex items-center justify-between py-3 px-5 bg-primary sticky top-0">
+        <motion.div
+          whileTap={{ scale: 0.75 }}
+          className="relative cursor-pointer"
+        >
           <MdShoppingBasket className="w-7 h-6 text-textColor" />
           <span className="absolute bottom-4 left-3 bg-cartNumBg w-6 h-6 text-center rounded-full text-white text-sm">
             2
           </span>
-        </div>
+        </motion.div>
 
         <div className="flex items-center gap-2 cursor-pointer">
           <img className="w-8 object-contain" src={Logo} alt="site logo" />
           <h1 className="text-xl font-bold">City</h1>
         </div>
 
-        <img
+        <motion.img
+          whileTap={{ scale: 0.75 }}
           className="w-10 object-contain cursor-pointer"
           src={Avatar}
           alt="user-avatar"
@@ -28,7 +33,7 @@ const Header = () => {
       </nav>
 
       {/* Laptop & Desktop Navbar*/}
-      <nav className="hidden md:flex justify-between py-5 px-12 bg-primary">
+      <nav className="hidden md:flex justify-between py-5 px-16 bg-primary sticky top-0">
         <div className="flex items-center gap-2 cursor-pointer">
           <img className="w-8 object-contain" src={Logo} alt="site logo" />
           <h1 className="text-xl font-bold">City</h1>
@@ -49,13 +54,17 @@ const Header = () => {
               <a href="/">Service</a>
             </li>
           </ul>
-          <div className="relative cursor-pointer">
+          <motion.div
+            whileTap={{ scale: 0.75 }}
+            className="relative cursor-pointer"
+          >
             <MdShoppingBasket className="w-7 h-6 text-textColor" />
             <span className="absolute bottom-4 left-3 bg-cartNumBg w-6 h-6 text-center rounded-full text-white text-sm">
               2
             </span>
-          </div>
-          <img
+          </motion.div>
+          <motion.img
+            whileTap={{ scale: 0.75 }}
             className="w-10 object-contain cursor-pointer"
             src={Avatar}
             alt="user-avatar"
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter as Router } from "react-router-dom"
 import "./index.css"
 import App from "./App"
+import { AnimatePresence } from "framer-motion"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AnimatePresence exitBeforeEnter>
+        <App />
+      </AnimatePresence>
     </Router>
   </React.StrictMode>
 )
