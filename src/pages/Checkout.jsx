a20b1ef3035a7f8cbc9556ba5d6e965882fad38d
krish-98 <muralikrishnanm98@gmail.com>
2022-11-01 14:26:55 +0530

    validating form using formik with yup

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,68 +1,155 @@
 import React from "react"
 import { useSelector } from "react-redux"
+import { useFormik } from "formik"
+import * as Yup from "yup"
 
 const Checkout = () => {
   const { cartItems, cartTotalAmount } = useSelector((state) => state.cart)
+  const radioOptions = [
+    { key: "Option 1", value: "value 1" },
+    { key: "Option 2", value: "value 2" },
+    { key: "Option 3", value: "value 3" },
+  ]
+  const formik = useFormik({
+    // Initial form values
+    initialValues: {
+      name: "",
+      address: "",
+      pincode: "",
+      city: "",
+      phoneNumber: "",
+      checked: "",
+    },
+
+    // Validate Form
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(20, "Name must be 20 characters or less.")
+        .required("Name is required"),
+      address: Yup.string().required("Address is required"),
+      pincode: Yup.string()
+        .min(6, "Pincode must be 6 digits")
+        .required("Pincode is required"),
+      city: Yup.string().required("City is required"),
+      phoneNumber: Yup.string()
+        .min(10, "Phone Number must have 10 digits")
+        .required("Phone Number is required"),
+    }),
+
+    // Submit Form
+    onSubmit: (values) => {
+      console.log("Submitted")
+    },
+  })
 
   return (
     <div className="h-[calc(100vh-80px)] flex flex-col items-center gap-8 pt-8 bg-primary">
       <h3 className="text-4xl font-semibold lg:text-5xl">Checkout</h3>
 
-      <form className="w-full p-4 md:w-[70%] lg:flex lg:items-center lg:gap-8 lg:justify-center lg:my-24">
-        <div className="flex flex-col gap-4 mb-8 lg:flex-1">
-          <div className="w-full flex justify-between items-center">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-full p-4 md:w-[70%] lg:flex lg:items-center lg:gap-8 lg:justify-center lg:my-24"
+      >
+        <div className="flex flex-col gap-10 mb-8 lg:flex-1">
+          <div className="w-full flex justify-between items-center relative">
             <label htmlFor="" className="w-[20%] text-right">
               Name
             </label>
+
             <input
               className="w-[75%] py-2 px-3 border-none focus:border-orange-400 focus:ring-2 focus:ring-orange-400 focus:bg-orange-100 placeholder:text-sm rounded-lg"
               type="text"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="Mr.Miller"
             />
+            <span className="absolute bottom-11 left-40 text-cartNumBg text-sm">
+              {formik.touched.name && formik.errors.name
+                ? formik.errors.name
+                : ""}
+            </span>
           </div>
 
-          <div className="w-full flex justify-between items-center">
+          <div className="w-full flex justify-between items-center relative">
             <label htmlFor="" className="w-[20%] text-right">
               Address
             </label>
             <input
               className="w-[75%] py-2 px-3 border-none focus:border-orange-400 focus:ring-2 focus:ring-orange-400 focus:bg-orange-100 placeholder:text-sm rounded-lg"
               type="text"
+              name="address"
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="432, st.thomas street"
             />
+            <span className="absolute bottom-11 left-40 text-cartNumBg text-sm">
+              {formik.touched.address && formik.errors.address
+                ? formik.errors.address
+                : ""}
+            </span>
           </div>
 
-          <div className="w-full flex justify-between items-center">
+          <div className="w-full flex justify-between items-center relative">
             <label htmlFor="" className="w-[20%] text-right">
               Pincode
             </label>
             <input
               className="w-[75%] py-2 px-3 border-none focus:border-orange-400 focus:ring-2 focus:ring-orange-400 focus:bg-orange-100 placeholder:text-sm rounded-lg"
               type="number"
+              name="pincode"
+              value={formik.values.pincode}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="501234"
             />
+            <span className="absolute bottom-11 left-40 text-cartNumBg text-sm">
+              {formik.touched.pincode && formik.errors.pincode
+                ? formik.errors.pincode
+                : ""}
+            </span>
           </div>
 
-          <div className="w-full flex justify-between items-center">
+          <div className="w-full flex justify-between items-center relative">
             <label htmlFor="" className="w-[20%] text-right">
               City
             </label>
             <input
               className="w-[75%] py-2 px-3 border-none focus:border-orange-400 focus:ring-2 focus:ring-orange-400 focus:bg-orange-100 placeholder:text-sm rounded-lg"
               type="text"
+              name="city"
+              value={formik.values.city}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="Mumbai"
             />
+            <span className="absolute bottom-11 left-40 text-cartNumBg text-sm">
+              {formik.touched.city && formik.errors.city
+                ? formik.errors.city
+                : ""}
+            </span>
           </div>
 
-          <div className="w-full flex justify-between items-center">
+          <div className="w-full flex justify-between items-center relative">
             <label htmlFor="" className="w-[20%] text-right">
               Phone Number
             </label>
             <input
               className="w-[75%] py-2 px-3 border-none focus:border-orange-400 focus:ring-2 focus:ring-orange-400 focus:bg-orange-100 placeholder:text-sm rounded-lg"
               type="number"
+              name="phoneNumber"
+              value={formik.values.phoneNumber}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="+91 9876543210"
             />
+            <span className="absolute bottom-11 left-40 text-cartNumBg text-sm">
+              {formik.touched.phoneNumber && formik.errors.phoneNumber
+                ? formik.errors.phoneNumber
+                : ""}
+            </span>
           </div>
         </div>
 
@@ -78,7 +165,7 @@ const Checkout = () => {
                 value="online"
                 id="online"
               />
-              <span className="ml-1">Online</span>
+              <span className="ml-2">Online</span>
             </label>
 
             <label htmlFor="cod">
@@ -89,7 +176,7 @@ const Checkout = () => {
                 value="cod"
                 id="cod"
               />
-              <span className="ml-1">COD</span>
+              <span className="ml-2">COD</span>
             </label>
           </div>
 
